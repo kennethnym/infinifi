91f1db90a3b2b674dbad7beaca65c9d6a63b0aec
Kenneth <kennethnym@outlook.com>
2024-07-20 16:28:16 +0100

    feat: gen more audio files and increase duration

--- generate.py
+++ generate.py
@@ -1,19 +1,32 @@
 import torchaudio
+import time
 from audiocraft.models.musicgen import MusicGen
 from audiocraft.data.audio import audio_write
 
 MODEL_NAME = "facebook/musicgen-large"
+MUSIC_DURATION_SECONDS = 60
 
 print(f"getting {MODEL_NAME}...")
 
 model = MusicGen.get_pretrained(MODEL_NAME)
-descriptions = ["gentle lo-fi beats"]
+model.set_generation_params(duration=MUSIC_DURATION_SECONDS)
+descriptions = [
+    "gentle, calming lo-fi beats that helps with studying and focusing",
+    "calm, piano lo-fi beats to help with studying and focusing",
+    "gentle lo-fi hip-hop to relax to",
+    "gentle, quiet synthwave lo-fi beats",
+    "morning lo-fi beats"
+]
 
 print("model obtained. generating wav files...")
 
+a = time.time()
+
 wav = model.generate(descriptions)
 
-print(f"{len(wav)} generated.")
+b = time.time()
+
+print(f"{len(wav)} generated. took {b - a} seconds.")
 
 for idx, one_wav in enumerate(wav):
     # Will save under {idx}.wav, with loudness normalization at -14 db LUFS.
